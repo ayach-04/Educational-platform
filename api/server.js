47e26bd5bbd2560f9dafaac921d8311e4582b380
Aya <ilainjouri@gmail.com>
2025-05-07 15:22:26 +0100

    Delete api directory

--- api/server.js
+++
@@ -1,8 +0,0 @@
-// This file serves as the entry point for Vercel serverless deployment
-// It simply re-exports the server from the server directory
-
-// Import the server app
-const serverApp = require('../server/server');
-
-// Export the server for Vercel
-module.exports = serverApp;
